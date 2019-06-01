66db1898ff3ee572c09c67107d28ea1a0bc8e880
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2019-06-01 21:09:38 +0200

    Initial Commit

---
+++ stringToDouble/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  stringToDouble
+//
+//  Created by Magnus Kruschwitz on 01.06.19.
+//  Copyright © 2019 Magnus Kruschwitz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
