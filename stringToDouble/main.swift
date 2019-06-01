f0183be5ace7926218fef4c3d21c38200d1787c3
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2019-06-01 21:32:51 +0200

    add function and output

--- stringToDouble/main.swift
+++ stringToDouble/main.swift
@@ -8,5 +8,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func stringToDouble (input: String) -> Double?{
+    let oFormatter = NumberFormatter()
+    if let result = oFormatter.number(from: input)?.doubleValue{
+        return result
+    }
+    else{
+        return nil
+    }
+}
 
+let aTestValues = ["15","A","45","nil","false","true","25"];
+
+for sTestValue in aTestValues{
+    print("Result = \(String(describing: stringToDouble(input: sTestValue)))")
+}
